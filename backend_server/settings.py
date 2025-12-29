386c4720cb64e367cd5135cffc918ddf09d8304e
Sameer251369 <sameerbashir522@gmail.com>
2025-12-30 00:15:55 +0530

    Fix admin.E403: Configured DjangoTemplates and APP_DIRS

--- backend_server/settings.py
+++ backend_server/settings.py
@@ -51,6 +51,21 @@ MIDDLEWARE = [
 ROOT_URLCONF = 'backend_server.urls'
 
 WSGI_APPLICATION = 'backend_server.wsgi.application'
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [],
+        'APP_DIRS': True, # This allows Django to find the admin templates
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
 
 # Updated Database Logic
 DATABASES = {
